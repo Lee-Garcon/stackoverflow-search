4cbffa217dd6277a847a93c6b670a16c67e24365
Lee Garcon <wallace.dylan00@gmail.com>
2019-06-26 11:42:10 +0900

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
     name="stackoverflow-search",
     version="0.1.0",
-    packages=["requests", "bs4"],
+    packages=["so_search"],
     entry_points={
         "console_scripts": ["so-search = stackoverflow_search.__main__:main"]
     },
+    install_requires=["requests", "bs4"],
 )
